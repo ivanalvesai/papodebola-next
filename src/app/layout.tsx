74d7d700940212844c07ea5d1f6637c7684bb640
Ivan Alves <ivansjp@gmail.com>
2026-04-16 14:47:05 -0300

    feat(seo): novo conjunto de favicons, og-image e GTM beforeInteractive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,12 +31,17 @@ export const metadata: Metadata = {
     siteName: "Papo de Bola",
     locale: "pt_BR",
     type: "website",
+    images: [{ url: "/og-image.jpg", width: 1200, height: 630 }],
   },
   twitter: {
     card: "summary_large_image",
   },
   icons: {
-    icon: "/favicon.svg",
+    icon: [
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
 };
 
@@ -48,7 +53,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${openSans.variable} antialiased`}>
       <head>
-        <Script id="gtm-head" strategy="afterInteractive">
+        <Script id="gtm-head" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MMRXG48R');`}
         </Script>
       </head>
